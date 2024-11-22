a31e5e52734234826d67a484274db2fda5e5f75c
Rafaela-Heer <rafaelaheer@hotmail.com>
2024-11-22 05:01:54 -0300

    fix/simulação

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.getElementById("calcular").addEventListener("click", function() {
+    
+    const materia_organica = parseFloat(document.getElementById("materia-organica").value);
+    const resultado = document.getElementById("resultado");
+
+    const energia_por_kg = 4.17;
+    const eficiencia = 0.8;
+    const tarifa_sp = 0.527;
+    const percentual_desconto = 0.5
+
+
+   
+    if (materia_organica > 0) {
+        const energia_obtida = materia_organica * energia_por_kg * eficiencia;
+        const valor_gerado = tarifa_sp * energia_obtida;
+        const desconto_total = valor_gerado * percentual_desconto;
+        resultado.innerHTML = `Com ${materia_organica} kg de biomassa, você estará gerando ${energia_obtida.toFixed(2)} KWh de energia. <br>
+        O seu desconto total será de R$ ${desconto_total.toFixed(2)} na conta de luz.`;
+    } else {
+        resultado.innerHTML = "Por favor, insira uma quantidade válida de matéria orgânica.";
+    }
+});

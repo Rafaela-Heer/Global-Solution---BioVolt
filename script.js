0cfd7bab7cf53f1d18061b09dc78c2d818894636
Julia Hadja <juliahadja@gmail.com>
2024-11-21 23:19:08 -0300

    feat-contatos

--- script.js
+++ script.js
@@ -29,3 +29,36 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+//Contatos
+function enviar(event){
+    event.preventDefault();
+    var nome = document.querySelector('#nome').value
+    var email = document.querySelector('#email').value
+    var numero = document.querySelector('#numero').value
+    var mensagem = document.querySelector('#mensagem').value
+    var erro = document.querySelector('#erro')
+
+    if(
+        (nome.length < 2)
+        
+    ){  erro.textContent = 'O nome precisa ter mais de 2 letras, preencha todos os campos.'
+        return
+    }
+    if(email.length == 0){
+        erro.textContent = 'Preencha todos os campos.'
+        return
+    }
+    if(numero.length == 0){
+        erro.textContent = 'Preencha todos os campos.'
+        return
+    }
+    if(mensagem.length == 0){
+        erro.textContent = 'Preencha todos os campos.'
+        return
+    }
+    erro.style.background='#06181C'
+    erro.textContent = 'Mensagem enviada com sucesso!'
+    erro.style.color= '#ffffff'
+
+}
